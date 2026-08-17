95f8127ed5a95dd2d6b9ccf88b7fd7fbd4edea9c
Nguyen Ngoc Thach <nguyenngocthach2301@gmail.com>
2026-08-17 17:23:31 +0700

    chore: edit drizzle config connect

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /src
 
 COPY package*.json ./
 
-RUN npm ci
+RUN npm install
 
 COPY . .
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
+import "dotenv/config";
 
-dotenv.config();
+const host = process.env.DATABASE_HOST || "localhost";
+const port = process.env.DATABASE_PORT || "5432";
+const user = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const database = process.env.POSTGRES_DB;
 
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: `postgresql://${user}:${password}@${host}:${port}/${database}`,
   },
 });
